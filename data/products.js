a6c9a735b7f7202a632481a5aff226af007058e7
Ntsetselelo <91235453+ntsetselelo@users.noreply.github.com>
2024-06-25 00:25:11 +0200

    move addtocart to jcartjs

--- data/products.js
+++ data/products.js
@@ -1,4 +1,4 @@
-const products = [{
+export const products = [{
   id: "e43638ce-6aa0-4b85-b27f-e1d07eb678c6",
   image:'images/products/nike-black-shorts.webp',
   name: 'Nike Woven Lined Flow Shorts - Black | Pavement - Pavement NZ',
