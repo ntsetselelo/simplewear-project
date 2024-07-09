0188a292cd7456fc6430c0d7f39c2a924f930441
Ntsetselelo <91235453+ntsetselelo@users.noreply.github.com>
2024-07-09 14:00:20 +0200

    create automated test fro addtocart

--- data/cart.js
+++ data/cart.js
@@ -1,16 +1,21 @@
-export let cart = 
-JSON.parse(localStorage.getItem('cart'));
+export let cart;
 
-if(!cart){
-    cart = [{
-        productId:'e43638ce-6aa0-4b85-b27f-e1d07eb678c6',
-        quantity:2,
-        deliveryOptionId: '1'
-    },{
-        productId:'e43638ce-6aa0-4b85-b27f-e1d07eb678c8',
-        quantity:1,
-        deliveryOptionId: '2'
-    }];
+loadFromStorage();
+
+export function loadFromStorage(){
+    cart = JSON.parse(localStorage.getItem('cart'));
+
+    if(!cart){
+        cart = [{
+            productId:'e43638ce-6aa0-4b85-b27f-e1d07eb678c6',
+            quantity:2,
+            deliveryOptionId: '1'
+        },{
+            productId:'e43638ce-6aa0-4b85-b27f-e1d07eb678c8',
+            quantity:1,
+            deliveryOptionId: '2'
+        }];
+    }
 }
 
 
