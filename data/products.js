908e1e1248edbffda6730619dca0b29bc9557df5
Ntsetselelo <91235453+ntsetselelo@users.noreply.github.com>
2024-07-25 02:13:35 +0200

    convert products into classes

--- data/products.js
+++ data/products.js
@@ -1,3 +1,4 @@
+import { formatCurrency } from "../scripts/utils/money.js";
 
 export function getProduct(productId){
   let matchingProduct;
@@ -11,6 +12,30 @@ export function getProduct(productId){
   return matchingProduct;
 }
 
+class Product {
+  id;
+  image;
+  name;
+  ratings;
+  priceCents;
+
+
+  constructor(productDetails){
+     this.id = productDetails.id;
+     this.image = productDetails.image;
+     this.name = productDetails.name;
+     this.rating = productDetails.rating;
+     this.priceCents = productDetails.priceCents;
+  }
+
+  getStarUrl(){
+    return `images/ratings/rating-${this.rating.stars * 10}.png`;
+  }
+
+  getPrice(){
+    return `R${formatCurrency(this.priceCents)}`;
+  }
+}
 
 export const products = [{
   id: "e43638ce-6aa0-4b85-b27f-e1d07eb678c6",
@@ -66,4 +91,7 @@ export const products = [{
       count: 87
   },
   priceCents: 160099
-}];
+}].map((productDetails) => {
+   return new Product(productDetails);
+});
+
